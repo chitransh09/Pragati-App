c9a50bf2009375c954db50bb3314e8a13066a671
Chitransh Chandrawanshi <echitransh16@gmail.com>
2023-05-01 02:28:59 +0530

    Added some features

--- app/src/main/java/com/myprojects/pragati/fragments/CustomLinkFragment.kt
+++ app/src/main/java/com/myprojects/pragati/fragments/CustomLinkFragment.kt
@@ -1,13 +1,29 @@
 package com.myprojects.pragati.fragments
 
+import android.content.ContentValues
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import com.myprojects.pragati.R
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.myprojects.pragati.adapters.CustomLinksAdapter
+import com.myprojects.pragati.databinding.FragmentCustomLinkBinding
+import com.myprojects.pragati.model.CustomLinks
 
 class CustomLinkFragment : Fragment() {
+
+    private lateinit var binding: FragmentCustomLinkBinding
+    private lateinit var adapter: CustomLinksAdapter
+
+    //    private lateinit var shimmerLayout: ShimmerFrameLayout
+    private val dataLists = mutableListOf<CustomLinks>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +35,68 @@ class CustomLinkFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_custom_link, container, false)
+        binding = FragmentCustomLinkBinding.inflate(inflater, container, false)
+
+        (activity as AppCompatActivity).setSupportActionBar(binding.customLinksToolbar)
+        (activity as AppCompatActivity).supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        // Remove the app name from the Toolbar
+        (activity as AppCompatActivity).supportActionBar!!.setDisplayShowTitleEnabled(false)
+
+        binding.customLinksToolbar.navigationIcon?.setTintList(null) // Set the toolbar navigation icon to use the default color
+
+        setHasOptionsMenu(true)
+
+        adapter = CustomLinksAdapter(dataLists) // Initialize RecyclerView adapter
+
+        binding.customLinksRecyclerView.adapter = adapter  // Set RecyclerView adapter
+
+        binding.customLinksRecyclerView.layoutManager =
+            LinearLayoutManager(context)  // Set RecyclerView layout manager
+
+        fetchData()
+
+        return binding.root
+    }
+
+    private fun fetchData() {
+
+        val db = Firebase.firestore
+        db.collection("customLink")
+            .get()
+            .addOnSuccessListener { documents ->
+                val items = mutableListOf<CustomLinks>()
+
+                for (document in documents) {
+                    val title = document.getString("title")
+                    val url = document.getString("url")
+                    val userEmail = document.getString("ownerEmail")
+                    val imageRef = document.getString("favIconUrl")
+
+                    if (title != null && url != null && imageRef != null) {
+                        val item = CustomLinks(id, imageRef, userEmail, title, url)
+                        items.add(item)
+                    }
+                }
+
+                adapter.items = items
+                adapter.notifyDataSetChanged()
+
+            }
+            .addOnFailureListener { exception ->
+                Log.w(ContentValues.TAG, "Error getting documents.", exception)
+                Toast.makeText(context, "Failed to fetch data", Toast.LENGTH_LONG).show()
+            }
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                requireActivity().supportFragmentManager.popBackStack()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/myprojects/pragati/fragments/MoreFragment.kt
+++ app/src/main/java/com/myprojects/pragati/fragments/MoreFragment.kt
@@ -3,15 +3,13 @@ package com.myprojects.pragati.fragments
 import SharedPrefManager
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.myprojects.pragati.R
 import com.myprojects.pragati.activities.LoginActivity
-import com.myprojects.pragati.databinding.FragmentAboutUsBinding
 import com.myprojects.pragati.databinding.FragmentMoreBinding
 
 
@@ -58,16 +56,24 @@ class MoreFragment : Fragment() {
             transaction?.commit()
         }
 
-        binding.faqCard.setOnClickListener {
-            val fragment = FaqsFragment()
+//        binding.faqCard.setOnClickListener {
+//            val fragment = FaqsFragment()
+//            val transaction = fragmentManager?.beginTransaction()
+//            transaction?.replace(R.id.frame, fragment)
+//            transaction?.addToBackStack(null)
+//            transaction?.commit()
+//        }
+
+        binding.favoriteCard.setOnClickListener {
+            val fragment = FavoritesFragment()
             val transaction = fragmentManager?.beginTransaction()
             transaction?.replace(R.id.frame, fragment)
             transaction?.addToBackStack(null)
             transaction?.commit()
         }
 
-        binding.favoriteCard.setOnClickListener {
-            val fragment = FavoritesFragment()
+        binding.customLinkCard.setOnClickListener {
+            val fragment = CustomLinkFragment()
             val transaction = fragmentManager?.beginTransaction()
             transaction?.replace(R.id.frame, fragment)
             transaction?.addToBackStack(null)
