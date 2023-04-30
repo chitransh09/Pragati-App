5bedb15ece19956894815bb01b2630ace8e54075
Chitransh Chandrawanshi <echitransh16@gmail.com>
2023-05-01 02:32:16 +0530

    Added file

---
+++ app/src/main/java/com/myprojects/pragati/model/CustomLinks.kt
@@ -0,0 +1,9 @@
+package com.myprojects.pragati.model
+
+data class CustomLinks(
+    val id: Int,
+    val favIconUrl: String?,
+    val ownerEmail: String?,
+    val title: String?,
+    val url: String?
+)
---
+++ app/src/main/java/com/myprojects/pragati/model/FavouritesWebsites.kt
@@ -0,0 +1,10 @@
+package com.myprojects.pragati.model
+
+data class FavouritesWebsites(
+    val id: Int,
+    val title: String?,
+    val description: String?,
+    val image: String?,
+    val link: String?,
+    val filter: String?
+)
